f5f1a15259432b2096b6ed40055b1d9e226179a8
Adi Ben Ami <adi.afriat@gmail.com>
2023-05-13 22:07:02 +0300

    added about page

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,5 +1,4 @@
 const { Link, NavLink } = ReactRouterDOM
-import { ReactFragment } from "react"
 
 export function AppHeader() {
 
@@ -9,25 +8,18 @@ export function AppHeader() {
             <div className="logo"><img src="../assets/img/logo.png" alt="" /></div>
         </Link>
 
-        {/* <Link to="/email">
-            <div ></div>
-        </Link> */}
-
-        {/* <Link to="/note">
-            <div className="header-logos note-logo"><img src="../assets/img/keep-logo.png" alt="" /></div>
-        </Link> */}
-     
         <nav>
             <NavLink to="/"></NavLink>
-            <img className="google-apps-icon-logo" src="../assets/img/google-apps-icon.svg"></img>
+            {/* <img className="google-apps-icon-logo" src="assets/img/google-apps-icon.svg"></img> */}
+
             <NavLink to="/about" className="header-logos about-us-logo">
-                <img src="../assets/img/about-us.jpg" alt="" />
+                <img src="assets/img/about-us.jpg" alt="" />
             </NavLink>
             <NavLink to="/email" className="header-logos email-logo">
-                <img src="../assets/img/Gmail-icon.png" alt="" />
+                <img src="assets/img/Gmail-icon.png" alt="" />
             </NavLink>
             <NavLink to="/note" className="header-logos note-logo">
-                <img src="../assets/img/keep-logo.png" alt="" />
+                <img src="assets/img/keep-logo.png" alt="" />
             </NavLink>
     
         </nav>

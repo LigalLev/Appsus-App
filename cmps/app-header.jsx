1631dc3e2b913e590dcd42ba7cdcbc7f41f1ed67
LigalLev <ligalelias@gmail.com>
2023-05-13 21:33:54 +0300

    trying again to fix img

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -19,15 +19,15 @@ export function AppHeader() {
      
         <nav>
             <NavLink to="/"></NavLink>
-            <img className="google-apps-icon-logo" src="../assets/img/google-apps-icon.svg"></img>
+            <img className="google-apps-icon-logo" src="./assets/img/google-apps-icon.svg"></img>
             <NavLink to="/about" className="header-logos about-us-logo">
-                <img src="../assets/img/about-us.jpg" alt="" />
+                <img src="./assets/img/about-us.jpg" alt="" />
             </NavLink>
             <NavLink to="/email" className="header-logos email-logo">
-                <img src="../assets/img/Gmail-icon.png" alt="" />
+                <img src="./assets/img/Gmail-icon.png" alt="" />
             </NavLink>
             <NavLink to="/note" className="header-logos note-logo">
-                <img src="../assets/img/keep-logo.png" alt="" />
+                <img src="./assets/img/keep-logo.png" alt="" />
             </NavLink>
     
         </nav>
